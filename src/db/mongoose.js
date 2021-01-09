4728d4d87c55c165869425fc8f6c798d72d92e89
Geekinfo11 <infogeek476@gmail.com>
2021-01-09 15:42:12 +0100

    Config mongodb for heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/shorten-urls',{useNewUrlParser: true, useUnifiedTopology: true, socketTimeoutMS: 2000})
+var uriString = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/shorten-urls';
+
+mongoose.connect(uriString,{useNewUrlParser: true, useUnifiedTopology: true, socketTimeoutMS: 2000})
